f159d6112ef7638afd9e481be006230ca7187171
Max Kulicke <mkulicke@gmail.com>
2020-01-20 17:21:53 -0500

    cleaned up just a touch

--- script.js
+++ script.js
@@ -6,78 +6,35 @@ $(document).ready(function () {
     function () {
       switch (this.id) {
         case 'plannerCard':
-          // $(".plannerContent").show();
           $('.plannerContent').fadeIn(300);
           break;
-        case "passwordCard":
-          // $(".passwordContent").show();
+        case 'passwordCard':
           $('.passwordContent').fadeIn(300);
           break;
-        case "product":
+        case 'tbd':
           break;
         default:
-          console.log("error");
+          console.log('error');
           break;
-       }
-    }, 
+      }
+    },
     function () {
       switch (this.id) {
         case 'plannerCard':
-          // $(".plannerContent").hide();
           $('.plannerContent').fadeOut(200);
           break;
-        case "passwordCard":
-          // $(".passwordContent").hide();
+        case 'passwordCard':
           $('.passwordContent').fadeOut(200);
           break;
-        case "product":
+        case 'tbd':
           break;
         default:
-          console.log("error");
+          console.log('error');
           break;
-       }
+      }
     }
- );
+  );
 
-//   $('div').hover(
-//     function () {
-//        $(this).css({"background-color":"red"});
-//     }, 
-//     function () {
-//        $(this).css({"background-color":"blue"});
-//     }
-//  );
-
-//  $( "li" ).hover(
-//   function() {
-//     $( this ).append( $( "<span> ***</span>" ) );
-//   }, function() {
-//     $( this ).find( "span" ).last().remove();
-//   }
-// );
- 
-// $( "li.fade" ).hover(function() {
-//   $( this ).fadeOut( 100 );
-//   $( this ).fadeIn( 500 );
-// });
-
-  const mouseover = (id) => {
-    switch (id) {
-      case "plannerCard":
-        $(".plannerContent").show();
-        break;
-      case "passwordCard":
-        $(".passwordContent").show();
-        break;
-      case "product":
-        break;
-      default:
-        console.log("error");
-        break;
-     }
-     
-  };
-
-//   const greeting = `My name is ${arya.first}!
-// I am loyal to ${arya.allegiance}.`;
+  //   const greeting = `My name is ${arya.first}!
+  // I am loyal to ${arya.allegiance}.`;
 })
