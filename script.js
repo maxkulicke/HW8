86feb068bd179d99473a0612b7c3b308dddc3634
Max Kulicke <mkulicke@gmail.com>
2020-01-20 19:45:17 -0500

    embedded spotify playlist

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 $(document).ready(function () {
 
-  $(".projectContent").hide();
+  $('.projectContent').hide();
+
+  $('.container').hover(
+    function () {
+      // console.log(this);
+      // console.log(this.id);
+      switch (this.id) {
+        case 'about':
+          $(this).animate({backgroundColor : 'rgb(66, 19, 236)'}, 'slow');
+          break;
+        case 'portfolio':
+          $(this).animate({backgroundColor : 'rgb(66, 19, 236)'}, 'slow');
+          break;
+        case 'music':
+          $(this).animate({backgroundColor : 'rgb(66, 19, 236)'}, 'slow');
+          break;
+        case 'photography':
+          $(this).animate({backgroundColor : 'rgb(66, 19, 236)'}, 'slow');
+          break;
+        case 'contact':
+          $(this).animate({backgroundColor : 'rgb(66, 19, 236)'}, 'slow');
+          break;
+        default:
+          console.log('error');
+          break;
+      }
+    },
+    function () {
+      switch (this.id) {
+        case 'about':
+          $(this).animate({ backgroundColor: 'rgba(255, 255, 255, 0.5)' });
+          break;
+        case 'portfolio':
+          $(this).animate({ backgroundColor: 'rgba(255, 255, 255, 0.5)' });
+          break;
+        case 'music':
+          $(this).animate({ backgroundColor: 'rgba(255, 255, 255, 0.5)' });
+          break;
+        case 'photography':
+          $(this).animate({ backgroundColor: 'rgba(255, 255, 255, 0.5)' });
+          break;
+        case 'contact':
+          $(this).animate({ backgroundColor: 'rgba(255, 255, 255, 0.5)' });
+          break;
+        default:
+          console.log('error');
+          break;
+      }
+    },
+  );
+
+  // const colorFadeIn = element => { $(element).animate(backgroundColor: rgb(244, 241, 255)); }
+
+  // const colorFadeOut = element => { $(element).animate(backgroundColor: rgb(244, 241, 255)); }
+
 
   $('.imgCard').hover(
     function () {
